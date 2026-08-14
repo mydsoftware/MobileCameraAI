9f382176b39dd294e47dee5201dd963b7062a2ff
mydsoftware <46159939+mydsoftware@users.noreply.github.com>
2026-08-14 15:55:27 +0330

    build: add Chaquopy Python runtime

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+    id("com.chaquo.python") version "17.0.0" apply false
 }
