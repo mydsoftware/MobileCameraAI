535179565058b0ae16ae0af61b02fda3f13bf567
mydsoftware <46159939+mydsoftware@users.noreply.github.com>
2026-08-14 14:24:16 +0330

    feat(android): model Uniview WebSocket camera streams

--- app/src/main/java/com/mydsoftware/mobilecameraai/CameraConfig.kt
+++ app/src/main/java/com/mydsoftware/mobilecameraai/CameraConfig.kt
@@ -3,15 +3,8 @@ package com.mydsoftware.mobilecameraai
 data class CameraConfig(
     val name: String,
     val host: String,
-    val onvifPort: Int,
-    val rtspPort: Int,
-    val streamPath: String = "/media/video1"
-)
-
-fun CameraConfig.rtspUri(username: String, password: String): String {
-    val safeUser = java.net.URLEncoder.encode(username, "UTF-8")
-        .replace("+", "%20")
-    val safePassword = java.net.URLEncoder.encode(password, "UTF-8")
-        .replace("+", "%20")
-    return "rtsp://$safeUser:$safePassword@$host:$rtspPort$streamPath"
+    val wsPort: Int,
+    val pathPrefix: String
+) {
+    fun wsPath(stream: Int): String = "$pathPrefix/flv/video$stream"
 }
